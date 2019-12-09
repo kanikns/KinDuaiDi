8600bc8bba4bed949206301a3bfdfa3b7ca9ee25
kani <59070021@it.kmitl.ac.th>
2019-12-09 20:36:39 +0700

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('findfriend/', include('findfriend.urls'))
+    path('findfriend/', include('findfriend.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
