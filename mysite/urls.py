1bfea0b8b3a233270c1aca2d541d84dc979762ff
kani <59070021@it.kmitl.ac.th>
2019-12-11 11:55:34 +0700

    .

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.static import static
 from django.urls import path
-from . import views
+from django.urls import views
+# from . import views
 from django.conf import settings
 
 urlpatterns = [
