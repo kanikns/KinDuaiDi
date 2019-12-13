14cc5c141fb57062501ec0940e8ae97e6fe8ffef
penwisalo <s30720@hotmail.com>
2019-12-13 14:30:02 +0700

    เพิ่มเบอร์

--- mysite/borrow/admin.py
+++ mysite/borrow/admin.py
@@ -6,7 +6,7 @@ class BorrowAdmin(admin.ModelAdmin):
     list_display = ['id', 'request_date', 'borrow_date', 'status', 'return_date']
 
 class Borrow_ItemAdmin(admin.ModelAdmin):
-    list_display = ['id', 'borrow_id', 'item_id', 'borrow_amount']
+    list_display = ['id', 'borrow_id', 'item_id', 'borrow_amount', 'borrow_tel']
 
 admin.site.register(Borrow, BorrowAdmin)
 admin.site.register(Borrow_Item, Borrow_ItemAdmin)
